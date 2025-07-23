52479c28c9eef6a0ae8da0a44292d1fec5ce94d9
kanishq-9 <purohitkanishq9@gmail.com>
2025-07-23 12:10:17 +0530

    added data and worked with async

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,45 @@
-const planets = [];
+const fs = require("fs");
+const path = require("path");
+const { parse } = require("csv-parse");
 
-module.exports = planets;
+const planets = [];
+function isHabitablePlanet(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+
+function loadPlanetsData() {
+  new Promise((resolve, reject) => {
+    fs.createReadStream(path.join(__dirname, "..", "data", "kepler_data.csv"))
+      .pipe(
+        parse({
+          comment: "#",
+          columns: true,
+        })
+      )
+      .on("data", (data) => {
+        if (isHabitablePlanet(data)) {
+          planets.push(data);
+        }
+      })
+      .on("error", (err) => {
+        reject(err);
+      })
+      .on("end", () => {
+        resolve();
+        console.log(planets.length)
+      });
+  });
+}
+
+module.exports = {
+  loadPlanetsData,
+  planets,
+};
+
+//the stream is an asynchronous, so whenever this module is called it loads the planet export before stream has completed because it is asychronous.. Hence we use promise
+//we then export loadPlanetData and in the server we wait
--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,18 @@
-const http = require("http");
+const { createServer } = require("http");
 const app = require("./app");
+const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 
-const server = http.createServer(app);
-server.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}...`);
-});
+const server = createServer(app);
+
+async function startServer() {
+  await loadPlanetsData();
+  server.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}...`);
+  });
+}
+startServer();
 
 //NOTE
 
